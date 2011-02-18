fefc1c50aacd2cf033174d93ba8535f3b11a24d8
Sergey Kryzhanovsky <skryzhanovsky@gmail.com>
2011-02-18 02:59:33 +0200

    Ongoing development

--- ccli.js
+++ ccli.js
@@ -1,7 +1,8 @@
 var fs = require('fs'),
     parse = require('./lib/cssp').parse,
+    minimize = require('./lib/cssm').minimize,
     utils = require('./lib/utils');
 
 if (process.argv.length === 3) {
-    console.log(utils.basic2string(parse(fs.readFileSync(process.argv[2]).toString()), ''));
+    console.log(utils.min2string(minimize(parse(fs.readFileSync(process.argv[2]).toString())).nodes, ''));
 }
