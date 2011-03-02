2d5624454c5f8460346757d5f4ad056dc93825cd
Sergey Kryzhanovsky <skryzhanovsky@gmail.com>
2011-03-02 14:05:35 +0200

    rollback to min2string

--- ccli.js
+++ ccli.js
@@ -4,7 +4,7 @@ var fs = require('fs'),
     utils = require('./lib/utils');
 
 if (process.argv.length === 3) {
-//    console.log(utils.min2string(minimize(parse(fs.readFileSync(process.argv[2]).toString())).nodes, ''));
-    console.log(utils.dump2string(minimize(parse(fs.readFileSync(process.argv[2]).toString())).nodes, ''));
+    console.log(utils.min2string(minimize(parse(fs.readFileSync(process.argv[2]).toString())).nodes, ''));
+//    console.log(utils.dump2string(minimize(parse(fs.readFileSync(process.argv[2]).toString())).nodes, ''));
 
 }
