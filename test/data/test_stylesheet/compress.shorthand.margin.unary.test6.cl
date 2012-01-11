01be22160d2633e1616b8739ca304446c1cbc9f3
Sergey Kryzhanovsky <skryzhanovsky@gmail.com>
2012-01-11 21:31:21 +0200

    issues/45: broken join in case of negative values in shorthands

---
+++ test/data/test_stylesheet/compress.shorthand.margin.unary.test6.cl
@@ -0,0 +1,1 @@
+a{margin-top:-0!important;margin-right:-1px!important;margin-bottom:-2px!important;margin-left:-3px!important}
--- web/csso.web.js
+++ web/csso.web.js
@@ -961,26 +961,33 @@ TRBL.extractMain = function(name) {
 
 TRBL.prototype.add = function(name, sValue, tValue) {
     var s = this.sides,
-        i, x, side, a = [];
+        i, x, side, a = [],
+        last;
     if ((i = name.lastIndexOf('-')) !== -1) {
         side = name.substr(i + 1);
         if (side in s) {
-            if (!s[side]) s[side] = { s: sValue, t: tValue[0] };
+            if (!s[side]) {
+                s[side] = { s: sValue, t: [tValue[0]] };
+                if (tValue[0][1] === 'unary') s[side].t.push(tValue[1]);
+            }
             return true;
         }
     } else if (name === this.name) {
         for (i = 0; i < tValue.length; i++) {
             x = tValue[i];
             switch(x[1]) {
+                case 'unary':
+                    a.push({ s: x[2], t: last = [x] });
+                    break;
                 case 'number':
                 case 'ident':
-                    a.push({ s: x[2], t: x });
+                    last = (last ? last.push(x) : a.push({ s: x[2], t: [x] }), null);
                     break;
                 case 'percentage':
-                    a.push({ s: x[2][2] + '%', t: x });
+                    last = (last ? last.push(x) : a.push({ s: x[2][2] + '%', t: [x] }), null);
                     break;
                 case 'dimension':
-                    a.push({ s: x[2][2] + x[3][2], t: x });
+                    last = (last ? last.push(x) : a.push({ s: x[2][2] + x[3][2], t: [x] }), null);
                     break;
                 case 's':
                 case 'comment':
@@ -1026,10 +1033,10 @@ TRBL.prototype.getValue = function() {
     }
 
     for (var i = 0; i < a.length - 1; i++) {
-        r.push(a[i].t);
+        r = r.concat(a[i].t);
         r.push([{ s: ' ' }, 's', ' ']);
     }
-    r.push(a[i].t);
+    r = r.concat(a[i].t);
 
     return r;
 };
