d54e5391487b5e06f31e516135e6b57d4a9d79fb
Roman Dvornov <rdvornov@gmail.com>
2016-02-27 02:26:30 +0300

    parser fixes, parse error tests

--- test/parse.js
+++ test/parse.js
@@ -1,32 +1,37 @@
-var fs = require('fs');
-var path = require('path');
 var assert = require('assert');
 var csso = require('../lib/index.js');
-var JsonLocator = require('./helpers/JsonLocator.js');
-
-function createParseTest(name, test, scope) {
-    it(name, function() {
-        var ast = csso.parse(test.source, scope);
-
-        // AST should be equal
-        assert.equal(csso.stringify(ast), csso.stringify(test.ast));
-
-        // translated AST should be equal to original source
-        assert.equal(csso.translate(ast), 'restoredSource' in test ? test.restoredSource : test.source);
-    });
-}
+var forEachParseTest = require('./fixture/parse').forEachTest;
 
 describe('parse', function() {
-    var testDir = path.join(__dirname, 'fixture/parse');
+    forEachParseTest(function createParseTest(name, test, scope) {
+        it(name, function() {
+            var ast = csso.parse(test.source, scope);
 
-    fs.readdirSync(testDir).forEach(function(rule) {
-        var filename = path.join(testDir, rule);
-        var tests = require(path.join(testDir, rule));
-        var scope = path.basename(rule, path.extname(rule));
-        var locator = new JsonLocator(filename);
+            // AST should be equal
+            assert.equal(csso.stringify(ast), csso.stringify(test.ast));
 
-        for (var name in tests) {
-            createParseTest(locator.get(name), tests[name], scope);
-        }
+            // translated AST should be equal to original source
+            assert.equal(csso.translate(ast), 'restoredSource' in test ? test.restoredSource : test.source);
+        });
+    });
+});
+
+describe('parse error', function() {
+    require('./fixture/parse-errors.json').forEach(function(test) {
+        it(test.css, function() {
+            var error;
+
+            assert.throws(function() {
+                csso.parse(test.css);
+            }, function(e) {
+                error = e;
+                if (e.parseError) {
+                    return true;
+                }
+            }, 'Should be CSS parse error');
+
+            assert.equal(error.message, test.error);
+            assert.deepEqual(error.parseError, test.position);
+        });
     });
 });
