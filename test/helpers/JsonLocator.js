1f59b9cf742d29e018c0d5a2e9f5ad711bfa70d5
XhmikosR <xhmikosr@gmail.com>
2019-01-06 01:04:19 +0200

    Use classes.

--- test/helpers/JsonLocator.js
+++ test/helpers/JsonLocator.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-function JsonLocator(filename) {
-    this.content = fs.readFileSync(filename, 'utf-8');
-    this.filename = path.relative(path.join(__dirname, '/../..'), filename);
-}
+class JsonLocator {
+    constructor(filename) {
+        this.content = fs.readFileSync(filename, 'utf-8');
+        this.filename = path.relative(path.join(__dirname, '/../..'), filename);
+    }
 
-JsonLocator.prototype.get = function(name) {
-    const lines = this.content.split(`"${name}"`)[0].split('\n');
-    return `${[
-        this.filename,
-        lines.length,
-        lines.pop().length + name.length + 5
-    ].join(':')} (${name})`;
-};
+    get(name) {
+        const lines = this.content.split(`"${name}"`)[0].split('\n');
+        return `${[
+            this.filename,
+            lines.length,
+            lines.pop().length + name.length + 5
+        ].join(':')} (${name})`;
+    }
+}
 
 module.exports = JsonLocator;
